c859e2be6507197ae550966bee21fff9fab92c2d
Martin Gergov <martingergov1@gmail.com>
2018-02-01 19:45:10 +0100

    new scheme, added open, close, stat(mmap to memory todo)

--- hello.asm
+++ hello.asm
@@ -1,17 +1,59 @@
-extern open
 extern mmap64
 
+section .bss
+    stat resb 144
+    fsize resb 4
 
+struc STAT
+    .st_dev         resq 1
+    .st_ino         resq 1
+    .st_nlink       resq 1
+    .st_mode        resd 1
+    .st_uid         resd 1
+    .st_gid         resd 1
+    .pad0           resb 4
+    .st_rdev        resq 1
+    .st_size        resq 1
+    .st_blksize     resq 1
+    .st_blocks      resq 1
+    .st_atime       resq 1
+    .st_atime_nsec  resq 1
+    .st_mtime       resq 1
+    .st_mtime_nsec  resq 1
+    .st_ctime       resq 1
+    .st_ctime_nsec  resq 1
+endstruc
+
+SECTION .data
+fname:	db "inject.o", 0
 SECTION .text
 	global _start
 _start:
-	push 2
-	push "inject.o"
-	call open
+	mov rax, 2
+	mov rdi, fname 
+	mov rsi, 2	; open inject code
+	syscall
+	mov r14, rax
+	
+	mov rdi, fname
+	mov rsi, stat
+	mov rax, 4	; stat inject code
+	syscall
+
+	mov eax, dword [stat + STAT.st_size]
+	mov DWORD [fsize], eax 	; assign file size
+
+	mov rdi, r14
+	mov rax, 3 ; close
+	syscall
+	;; mov eax, dword [stat + STAT.st_size]
+	;; mov rbx, rax
+	;; mov rax, 9	; mmap
+	;; mov 
+	;; syscall
 	;; mmap
 	;; jmp addr ; jmp to mmap-ed instructions here
-	;; 
-	jmp    _start
-        xor    ebx, ebx 	; 0
-	mov	eax, 1
-        int    0x80
+	;; jmp    _start
+        xor    	rbx, rbx 	; 0
+	mov	rax, 1
+        int	0x80
